61bef93eee097f869564ce130c6566d410bc97bd
sualih <sualihmohammed53@gmail.com>
2025-11-29 11:16:32 +0300

    changes on sualih branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from 'react'
 
-// const App = () => {
-//   return (
-//     <div>
+const App = () => {
+  return (
+    <div>
       
-//     </div>
-//   )
-// }
+    </div>
+  )
+}
 
-// export default App
+export default App
