80ee474894467841683542dd61e9ecd8bc516619
sualih <sualihmohammed53@gmail.com>
2025-12-05 10:26:19 +0300

    updated

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Stats = ({ stats }) => {
   return (
-    
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4">
+      
+    </div>
   );
 };
 
