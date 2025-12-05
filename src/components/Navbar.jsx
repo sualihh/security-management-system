63acbc54458a881aa271baaefb393d223822ec34
sualih <sualihmohammed53@gmail.com>
2025-12-05 10:09:34 +0300

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,7 @@ const Navbar = () => {
   
   {/* Left Logo */}
   <div className="flex-1">
-    <a className="text-xl font-semibold cursor-pointer">
-      <img
-          className="w-10 h-10 object-cover"
-          src={assets.wollo_logo}
-          alt="smart"
-        />WOLLO</a>
+    
   </div>
 
   {/* Right Side */}
