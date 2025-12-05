c6c97a1403fbe54ae33a95c7844fdd3d875da91c
sualih <sualihmohammed53@gmail.com>
2025-12-05 10:21:51 +0300

    updated

--- src/components/QRCodeScanner.jsx
+++ src/components/QRCodeScanner.jsx
@@ -42,7 +42,12 @@ const QRCodeScanner = ({ setRecentScans, setAlerts }) => {
   return (
     <div className="flex flex-col items-center justify-center">
       {!scanning && (
-        
+        <button
+          onClick={() => setScanning(true)}
+          className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 mb-4"
+        >
+          Start Scan
+        </button>
       )}
 
       {scanning && (
