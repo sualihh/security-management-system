84807b85a5c335c0f27adbc5f11b53bbef80903a
sualih <sualihmohammed53@gmail.com>
2025-12-05 10:24:08 +0300

    updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,13 @@ import {Link, NavLink} from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div className='h-screen border-r-4 border-gray-300 bg-white'>
-        
+        <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
+            
+            
+            
+            
+            
+        </div>
     </div>
   )
 }
