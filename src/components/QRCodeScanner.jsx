23eb84f0ca66a119e14d87200cfc11f32cbe22c1
sualih <sualihmohammed53@gmail.com>
2025-12-06 16:49:45 +0300

    updated

--- src/components/QRCodeScanner.jsx
+++ src/components/QRCodeScanner.jsx
@@ -41,11 +41,7 @@ const QRCodeScanner = ({ setRecentScans, setAlerts }) => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      
-
-      
-    </div>
+    
   );
 };
 
