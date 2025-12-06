398e37affc1fa049ce0d2b01be83d92b6804fe4e
sualih <sualihmohammed53@gmail.com>
2025-12-06 17:00:36 +0300

    updated

--- src/components/QRCodeScanner.jsx
+++ src/components/QRCodeScanner.jsx
@@ -70,6 +70,7 @@ const QRCodeScanner = ({ setRecentScans, setAlerts }) => {
             onClick={() => setScanning(false)}
             className="mt-4 bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
           >
+            Stop Scan
           </button>
 
           
