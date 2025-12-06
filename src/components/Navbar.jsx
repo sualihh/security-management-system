354439260a0f05d150af4ca6d3f61ddebe0912a3
sualih <sualihmohammed53@gmail.com>
2025-12-06 14:37:27 +0300

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,9 +42,7 @@ const Navbar = () => {
       <ul
         className="absolute right-0 mt-2 w-40 bg-white border rounded shadow-lg hidden group-focus-within:block group-hover:block"
       >
-        <li className="px-4 py-2 hover:bg-gray-100 flex justify-between cursor-pointer">
-          
-        </li>
+        
 
         
 
