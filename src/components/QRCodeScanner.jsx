498d379184363ed740d196df5577a1a077eec48c
sualih <sualihmohammed53@gmail.com>
2025-12-06 16:47:09 +0300

    updated

--- src/components/QRCodeScanner.jsx
+++ src/components/QRCodeScanner.jsx
@@ -55,9 +55,7 @@ const QRCodeScanner = ({ setRecentScans, setAlerts }) => {
         <div className="flex flex-col items-center w-full">
           {/* Live Camera Feed */}
           <div className="w-full max-w-md border rounded-lg overflow-hidden shadow-lg">
-            <QrReader
-             
-            />
+            
           </div>
 
           
