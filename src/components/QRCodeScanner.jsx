0968c516bb2ec7890a25cadc09e07da6d23d36b1
sualih <sualihmohammed53@gmail.com>
2025-12-06 16:53:54 +0300

    updated

--- src/components/QRCodeScanner.jsx
+++ src/components/QRCodeScanner.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { QrReader } from "@blackbox-vision/react-qr-reader";
 
-const QRCodeScanner = ({ setRecentScans, setAlerts }) => {
+const QRCodeScanner = () => {
 
   
   
